f32ffb451fd22d78a3699538ac8e2475cf9f504c
perezcleidy069-sketchh <perezcleidy069@gmail.com>
2026-03-09 14:10:51 -0600

    feat: :sparkles: Functions
    Add functions structure

---
+++ main.py
@@ -0,0 +1,23 @@
+def ask_option():
+    pass
+
+def separator():
+    pass
+
+def show_menu():
+    pass
+
+def add_product():
+    pass
+
+def show_product():
+    pass
+
+def update_product():
+    pass
+
+def delete_product():
+    pass
+
+def calculate_inventary():
+    pass
