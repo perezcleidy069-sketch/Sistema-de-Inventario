5a1d17f5ad85b569045fded471066d4d71a402eb
perezcleidy069-sketchh <perezcleidy069@gmail.com>
2026-03-09 14:59:06 -0600

    feat: :sparkles: add menu feature
    add menu features with conditional logic

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 def ask_option():
-    pass
+
+    opc=int(input("Ingrese la opcion deseada: "))
+    return opc
+
 
 def separator():
-    pass
+
+    print("* * 20")
 
 def show_menu():
-    pass
+    separator()
+    print("Bienvenido a la TIENDA LA LOLITA")
+    separator()
+    MENU=""""
+    1. Agregar productos
+    2. Mostrar productos
+    3. Actualizar cantidad de un producto
+    4. Eliminar productos
+    5. Calcular inventario
+    6. Salir"""
+    print(MENU)
+    separator()
+
 
 def add_product():
     pass
@@ -21,3 +37,25 @@ def delete_product():
 
 def calculate_inventary():
     pass
+
+while True:
+    show_menu()
+    try:
+        opc=ask_option()
+    except Exception:
+        print("Hubo un error con su opcion")
+        
+    if opc == 1:
+        add_product()
+    elif opc== 2:
+        show_product()
+    elif opc == 3:
+        update_product()
+    elif opc == 4:
+        delete_product()
+    elif opc== 5:
+        calculate_inventary()
+    elif opc== 6:
+        print("Adios, fue un gusto para nosotros que usted nos prefiriera :)")
+    else:
+        print("Ingresó la opcion incorrecta")    
